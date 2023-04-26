092dde492f5e9224e223a0c830f9a1302bd9ead5
Hugues Maignol <hugues@kime.tech>
2023-04-26 19:42:22 +0200

    qa: lint fix

--- apps/web/src/utils/runPromisesInChunks.ts
+++ apps/web/src/utils/runPromisesInChunks.ts
@@ -2,13 +2,10 @@ import { chunk } from 'lodash'
 import { runPromisesSequentially } from '@app/web/utils/runPromisesSequentially'
 
 export const runPromisesInChunks = async <T>(
-  promises: Iterable<T | PromiseLike<T>>,
+  promises: ArrayLike<T | PromiseLike<T>>,
   chunkSize: number,
 ): Promise<Awaited<T>[]> => {
-  const chunked = chunk(
-    Array.isArray(promises) ? promises : [...promises],
-    chunkSize,
-  )
+  const chunked = chunk(promises, chunkSize)
 
   const chunkedResults = await runPromisesSequentially(
     chunked.map((promisesChunk) => Promise.all(promisesChunk)),
