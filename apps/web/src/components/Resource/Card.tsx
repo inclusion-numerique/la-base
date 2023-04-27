56b7c630fbe41289ea96a84e8f5c56bc3561a2e0
Desoindx <xavier.desoindre@hotmail.fr>
2023-04-27 17:25:13 +0200

    fix style

--- apps/web/src/components/Resource/Card.tsx
+++ apps/web/src/components/Resource/Card.tsx
@@ -26,7 +26,7 @@ const ResourceCard = ({
           </Link>
         </span>
       </div>
-      <div className="fr-hidden fr-unhidden-lg fr-text--xs fr-mb-0">
+      <div className="fr-hidden fr-unhidden-md fr-text--xs fr-mb-0">
         Modifié le {dateAsDay(resource.created)}
       </div>
     </div>
@@ -35,14 +35,14 @@ const ResourceCard = ({
         <h6 className={styles.title}>{resource.title}</h6>
         <div className="fr-text--sm fr-mb-0">{resource.description}</div>
       </div>
-      <div className="fr-hidden-lg fr-text--xs fr-mb-1w">
+      <div className="fr-hidden-md fr-text--xs fr-mb-1w">
         Modifié le {dateAsDay(resource.created)}
       </div>
       {withImage && (
         <img className={styles.image} src="https://fakeimg.pl/140x80/" alt="" />
       )}
     </div>
-    <Badge className="fr-hidden-lg fr-mt-1w" noIcon severity="success">
+    <Badge className="fr-hidden-md fr-mt-1w" noIcon severity="success">
       Très recommandée
     </Badge>
     <div className={styles.footer}>
@@ -50,16 +50,16 @@ const ResourceCard = ({
         <span className="fr-icon-eye-line fr-icon--sm" />
         <div>
           <b>45</b>
-          <span className="fr-hidden fr-unhidden-lg"> Vues</span>
+          <span className={styles.spanMdDisplay}> Vues</span>
         </div>
         <div>.</div>
         <span className="fr-icon-bookmark-line fr-icon--sm" />
         <div>
           <b>45</b>
-          <span className="fr-hidden fr-unhidden-lg"> Enregistrements</span>
+          <span className={styles.spanMdDisplay}> Enregistrements</span>
         </div>
         <Badge
-          className="fr-hidden fr-unhidden-lg fr-mb-1w"
+          className="fr-hidden fr-unhidden-md fr-mb-1w"
           noIcon
           severity="success"
         >
@@ -68,14 +68,26 @@ const ResourceCard = ({
       </div>
       <div className={classNames(styles.footerRight, 'fr-text--sm', 'fr-mb-0')}>
         {connected && (
-          <Link className={classNames('fr-link', styles.iconLink)} href="/">
+          <Link
+            title="Editer"
+            className={classNames('fr-link', styles.iconLink)}
+            href="/"
+          >
             <span className="fr-icon-edit-line fr-icon--sm" />
           </Link>
         )}
-        <Link className={classNames('fr-link', styles.iconLink)} href="/">
+        <Link
+          title="Mettre en favoris"
+          className={classNames('fr-link', styles.iconLink)}
+          href="/"
+        >
           <span className="fr-icon-bookmark-line fr-icon--sm" />
         </Link>
-        <Link className={classNames('fr-link', styles.iconLink)} href="/">
+        <Link
+          title="Partager"
+          className={classNames('fr-link', styles.iconLink)}
+          href="/"
+        >
           <span className="fr-icon-links-line fr-icon--sm" />
         </Link>
       </div>
