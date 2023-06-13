895206c14eda5ae984014952177be9bb94abb105
Hugues Maignol <hugues@kime.tech>
2023-06-13 19:35:46 +0200

    fix: broken dsfr prod build import order

--- apps/web/src/app/PreloadResources.tsx
+++ apps/web/src/app/PreloadResources.tsx
@@ -6,23 +6,9 @@ import ReactDOM from 'react-dom'
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
 const Rd = ReactDOM as any
 
-const stylesToInit = [
-  '/dsfr/dsfr.min.css',
-  '/dsfr/utility/utility.min.css',
-  'https://cdn.jsdelivr.net/npm/remixicon@3.3.0/fonts/remixicon.css',
-]
 const fontsToPreload = ['Marianne-Regular', 'Marianne-Bold', 'Marianne-Medium']
 
 export const PreloadResources = () => {
-  for (const style of stylesToInit) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    Rd.preload(style, {
-      as: 'style',
-      type: 'text/css',
-      crossOrigin: 'anonymous',
-    })
-  }
-
   for (const font of fontsToPreload) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     Rd.preload(`/dsfr/fonts/${font}.woff2`, {
