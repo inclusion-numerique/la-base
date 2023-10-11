9aef1ed6ffd4fff76f7dac721fe2f521dd47d032
Hugues Maignol <hugues@kime.tech>
2023-10-11 09:24:22 +0200

    chore: split search migrations for ci bug

--- apps/web/prisma/migrations/20231010132236_search/migration.sql
+++ apps/web/prisma/migrations/20231010132236_search/migration.sql
@@ -1,6 +1,3 @@
--- Search extension and indexes
-CREATE EXTENSION IF NOT EXISTS unaccent;
-
 CREATE INDEX resources_search ON resources USING gin(to_tsvector('french', unaccent(title || ' ' || description)));
 CREATE INDEX bases_search ON bases USING gin(to_tsvector('french', unaccent(title || ' ' || coalesce(description,''))));
 CREATE INDEX profile_search ON users USING gin(to_tsvector('french', unaccent(coalesce(name, '') || ' ' || coalesce(location, '') ||  ' ' || coalesce(title, '') ||  ' ' || coalesce(description, ''))));
