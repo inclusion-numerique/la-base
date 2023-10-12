25ae451f15d7659fc1e3740ddcdcb80cec5b4ac2
Hugues Maignol <hugues@kime.tech>
2023-10-12 14:07:02 +0200

    fix: home public count

--- apps/web/src/server/resources/getResourcesList.ts
+++ apps/web/src/server/resources/getResourcesList.ts
@@ -96,6 +96,7 @@ export const getResourcesCountByTheme = async () => {
   >`
       SELECT unnest(themes) AS theme, CAST(COUNT(*) AS integer) AS "count"
       FROM resources
+      WHERE resources.is_public = true
       GROUP BY theme
       ORDER BY theme ASC;
   `
