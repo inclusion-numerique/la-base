8c290a23747f84c0626f275112027643f8101db5
Hugues Maignol <hugues@kime.tech>
2024-05-31 15:51:31 +0200

    fix: pgvector extension for ci env

--- apps/web/prisma/migrations/20240320113808_pgvector_extension/migration.sql
+++ apps/web/prisma/migrations/20240320113808_pgvector_extension/migration.sql
@@ -1,1 +1,13 @@
-CREATE EXTENSION IF NOT EXISTS "vector";
+DO $$
+    BEGIN
+        -- Check if 'pgvector' is available
+        IF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'pgvector') THEN
+            -- Create 'pgvector' extension if it is available
+            RAISE NOTICE 'Installing pgvector extension';
+            EXECUTE 'CREATE EXTENSION IF NOT EXISTS pgvector';
+        ELSE
+            -- Fallback to 'vector' if 'pgvector' is not available
+            RAISE NOTICE 'pgvector not available, installing vector extension instead';
+            EXECUTE 'CREATE EXTENSION IF NOT EXISTS vector';
+        END IF;
+    END $$;
