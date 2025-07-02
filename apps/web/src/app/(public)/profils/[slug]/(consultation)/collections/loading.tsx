bac4486dd114771f7dad6e23fae2e4fa3f030730
Kevin Gallet <kevingallet@gmail.com>
2025-07-02 10:40:04 +0200

    feat: harmonize titles profiles and bases (#282)
    * feat: review collection ux ui
    
    * feat: harmonize titles profiles and bases

--- apps/web/src/app/(public)/profils/[slug]/(consultation)/collections/loading.tsx
+++ apps/web/src/app/(public)/profils/[slug]/(consultation)/collections/loading.tsx
@@ -1,10 +1,16 @@
 import CollectionCardSkeleton from '@app/web/components/Collection/Cards/CollectionCardSkeleton'
+import IconInSquare from '@app/web/components/IconInSquare'
 import React from 'react'
 
 const LoadingProfileCollectionsPage = () => (
   <div data-testid="base-resources">
     <div className="fr-mb-6w fr-flex fr-justify-content-space-between">
-      <h2 className="fr-mb-0 fr-h3">Collections</h2>
+      <div className="fr-flex fr-align-items-center fr-flex-gap-5v">
+        <IconInSquare iconId="ri-folder-2-line" />
+        <h2 className="fr-mb-0 fr-h3 fr-text-label--blue-france">
+          Mes collections
+        </h2>
+      </div>
     </div>
     <div className="fr-grid-row fr-grid-row--gutters">
       {Array.from({ length: 4 }).map((_, index) => (
