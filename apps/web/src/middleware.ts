deb57da78e99946c804d4c6216b8d1f543916215
Hugues Maignol <hugues@maignol.com>
2025-08-25 14:12:08 +0200

    fix: matomo connect-src CSP
    (cherry picked from commit 9f284059a6713ed396c031bc347bbc70e61eee72)

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -24,7 +24,7 @@ const contentSecurityPolicy = `
   object-src 'none';
   connect-src 'self' blob: https://${ServerWebAppConfig.S3.uploadsBucket}.${
     ServerWebAppConfig.S3.host
-  } https://${PublicWebAppConfig.ProConnect.hostname} https://sentry.incubateur.anct.gouv.fr/ https://openmaptiles.geo.data.gouv.fr https://openmaptiles.github.io https://aides-territoires.beta.gouv.fr https://recherche-entreprises.api.gouv.fr https://api-adresse.data.gouv.fr;
+  } https://${PublicWebAppConfig.ProConnect.hostname} https://matomo.incubateur.anct.gouv.fr https://sentry.incubateur.anct.gouv.fr/ https://openmaptiles.geo.data.gouv.fr https://openmaptiles.github.io https://aides-territoires.beta.gouv.fr https://recherche-entreprises.api.gouv.fr https://api-adresse.data.gouv.fr;
   worker-src 'self' blob:;
   font-src 'self' https: data:;
   frame-ancestors 'self' https://matomo.incubateur.anct.gouv.fr;
