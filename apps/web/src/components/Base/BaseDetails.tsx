1a701d56b8b9088f1a56d437137e077826fbf8e6
Thibault <thibault.rouveyrol@gmail.com>
2025-09-04 17:18:53 +0200

    fix: title hierarchy in base page

--- apps/web/src/components/Base/BaseDetails.tsx
+++ apps/web/src/components/Base/BaseDetails.tsx
@@ -6,7 +6,7 @@ const BaseDetails = ({ base }: { base: BasePageData }) => (
   <div className="fr-width-full">
     <div className="fr-grid-row fr-grid-row--gutters fr-mb-4w">
       <div className="fr-col-12 fr-col-lg-8 fr-mt-3w">
-        <h3 className="fr-h6">Description</h3>
+        <h2 className="fr-h6">Description</h2>
         <div>
           {base.description ? (
             <div
