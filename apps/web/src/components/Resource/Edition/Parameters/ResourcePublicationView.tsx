653071c697dc6846e977cba4de7b4a442b6e6a3d
Kevin GALLET <kevingallet@gmail.com>
2025-09-05 10:31:54 +0200

    feat: contributor cant change resource locatoin anymore

--- apps/web/src/components/Resource/Edition/Parameters/ResourcePublicationView.tsx
+++ apps/web/src/components/Resource/Edition/Parameters/ResourcePublicationView.tsx
@@ -6,33 +6,53 @@ import {
 } from '@app/web/components/PrivacyTags'
 import RoundProfileImage from '@app/web/components/RoundProfileImage'
 import type { ResourceProjectionWithContext } from '@app/web/server/resources/getResourceFromEvents'
+import Notice from '@codegouvfr/react-dsfr/Notice'
 import styles from './ResourcePublicationView.module.css'
 
 const ResourcePublicationView = ({
+  isCreator,
   resource,
   user,
 }: {
+  isCreator: boolean
   resource: ResourceProjectionWithContext
   user: SessionUser
-}) =>
-  resource.base ? (
-    <div className={styles.baseInfo}>
-      <div className={styles.baseName}>
-        <BaseImage className="fr-mr-1w" base={resource.base} />
-        <span>{resource.base.title}</span>
-      </div>
-      <BasePrivacyTag isPublic={resource.base.isPublic} />
-    </div>
-  ) : (
-    resource.createdBy && (
-      <div className={styles.baseInfo}>
-        <div className={styles.baseName}>
-          <RoundProfileImage className="fr-mr-1w" user={resource.createdBy} />
-          {resource.createdBy.name}
-        </div>
-        <ProfilePrivacyTag isPublic={user.isPublic} />
-      </div>
-    )
+}) => {
+  const isContributor = resource.contributors.some(
+    (contributor) => contributor.contributorId === user.id,
   )
+  return (
+    <>
+      {(!!isContributor || !isCreator) && (
+        <Notice
+          className="fr-mb-3w"
+          title="En tant que contributeur, vous ne pouvez pas modifier le lieu de publication de la ressource."
+        />
+      )}
+      {resource.base ? (
+        <div className={styles.baseInfo}>
+          <div className={styles.baseName}>
+            <BaseImage className="fr-mr-1w" base={resource.base} />
+            <span>{resource.base.title}</span>
+          </div>
+          <BasePrivacyTag isPublic={resource.base.isPublic} />
+        </div>
+      ) : (
+        resource.createdBy && (
+          <div className={styles.baseInfo}>
+            <div className={styles.baseName}>
+              <RoundProfileImage
+                className="fr-mr-1w"
+                user={resource.createdBy}
+              />
+              {resource.createdBy.name}
+            </div>
+            <ProfilePrivacyTag isPublic={user.isPublic} />
+          </div>
+        )
+      )}
+    </>
+  )
+}
 
 export default ResourcePublicationView
