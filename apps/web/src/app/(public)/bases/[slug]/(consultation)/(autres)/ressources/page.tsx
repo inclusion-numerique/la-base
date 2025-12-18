cde37420ae8c3e17172537b5f20cf3dc6ecb8f51
Kevin GALLET <kevingallet@gmail.com>
2025-12-18 16:50:25 +0100

    fix: display base tab admin role

--- apps/web/src/app/(public)/bases/[slug]/(consultation)/(autres)/ressources/page.tsx
+++ apps/web/src/app/(public)/bases/[slug]/(consultation)/(autres)/ressources/page.tsx
@@ -14,19 +14,26 @@ const BaseResourcesPage = async ({
     base,
   } = await getBasePageContext(slug)
 
-  const { resources, id } = base
+  const { resources, id, members, createdById } = base
+
+  const isAdmin = user?.role === 'Admin'
+  const isBaseCreator = user?.id === createdById
+  const isBaseMember = members.some(
+    (member) => member.memberId === user?.id && member.accepted,
+  )
+
+  const canWrite = isAdmin
+    ? isBaseCreator || isBaseMember
+    : hasPermission('WriteBase')
 
   return resources.length === 0 ? (
-    <EmptyBaseResources
-      canCreateResource={hasPermission('WriteBase')}
-      baseId={id}
-    />
+    <EmptyBaseResources canCreateResource={canWrite} baseId={id} />
   ) : (
     <Resources
       title="Ressources"
       resources={resources}
       user={user}
-      canWrite={hasPermission('WriteBase')}
+      canWrite={canWrite}
       baseId={id}
     />
   )
