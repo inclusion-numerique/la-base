1b9a0d9fe4f057b08129e1186774145c453165af
Kevin GALLET <kevingallet@gmail.com>
2025-12-18 11:09:53 +0100

    fix: admin resource actions base member / not base member - base resource or not

--- apps/web/src/components/Resource/List/ResourceTab.tsx
+++ apps/web/src/components/Resource/List/ResourceTab.tsx
@@ -24,18 +24,29 @@ const ResourceTab = ({
     {resources.length === 0 ? (
       <div className={styles.emptyBox}>{emptyText}</div>
     ) : (
-      resources.map((resource) => (
-        <ResourceCard
-          titleAs="h3"
-          key={resource.slug}
-          isContributor={resourceAuthorization(resource, user).hasPermission(
-            ResourcePermissions.WriteResource,
-          )}
-          resource={resource}
-          user={user}
-          isDraft={isDraft}
-        />
-      ))
+      resources.map((resource) => {
+        const isAdmin = user?.role === 'Admin'
+        const resourceCreator = user?.id === resource.createdById
+        const isBaseMember = resource.base?.members.some(
+          (member) => member.memberId === user?.id && member.accepted,
+        )
+
+        const isContributor = isAdmin
+          ? resourceCreator || (!!resource.baseId && !!isBaseMember)
+          : resourceAuthorization(resource, user).hasPermission(
+              ResourcePermissions.WriteResource,
+            )
+        return (
+          <ResourceCard
+            titleAs="h3"
+            key={resource.slug}
+            isContributor={isContributor}
+            resource={resource}
+            user={user}
+            isDraft={isDraft}
+          />
+        )
+      })
     )}
   </div>
 )
