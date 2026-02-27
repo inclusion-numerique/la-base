955c39a6079e35a0d0df67cedeae3c4bfcdcab6d
Kevin GALLET <kevingallet@gmail.com>
2026-02-27 15:34:56 +0100

    fix: index resources

--- packages/ui/src/components/Form/SelectOptionsList.tsx
+++ packages/ui/src/components/Form/SelectOptionsList.tsx
@@ -1,3 +1,4 @@
+import styles from '@app/ui/components/Form/Filters/SearchFilter.module.css'
 import SearchFilterOption from '@app/ui/components/Form/Filters/SearchFilterOption'
 import type { SelectOption } from '@app/ui/components/Form/utils/options'
 
@@ -12,19 +13,21 @@ const SelectOptionsList = ({
   selectedOptions: SelectOption[]
   onClick: (option: SelectOption) => void
 }) => (
-  <>
-    {options.map((option) => (
-      <SearchFilterOption
-        data-testid={`${dataTestId}-${option.value}`}
-        key={option.value}
-        option={option}
-        selected={selectedOptions.some(
-          (selectedOption) => selectedOption.value === option.value,
-        )}
-        onSelect={(o) => onClick(o)}
-      />
+  <ul className="fr-raw-list">
+    {options.map((option, index) => (
+      <li className={styles.optionListItem} key={option.value}>
+        <SearchFilterOption
+          data-testid={`${dataTestId}-${option.value}`}
+          id={`select-options-list-${index}`}
+          option={option}
+          selected={selectedOptions.some(
+            (selectedOption) => selectedOption.value === option.value,
+          )}
+          onSelect={(o) => onClick(o)}
+        />
+      </li>
     ))}
-  </>
+  </ul>
 )
 
 export default SelectOptionsList
