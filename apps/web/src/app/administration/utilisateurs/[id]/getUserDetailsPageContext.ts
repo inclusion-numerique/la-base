c427508d32b79202dcb09cdad9ad615c43de91dd
Kevin GALLET <kevingallet@gmail.com>
2026-03-18 10:13:15 +0100

    feat: optim bo - user resources list

--- apps/web/src/app/administration/utilisateurs/[id]/getUserDetailsPageContext.ts
+++ apps/web/src/app/administration/utilisateurs/[id]/getUserDetailsPageContext.ts
@@ -2,41 +2,90 @@ import { prismaClient } from '@app/web/prismaClient'
 import { cache } from 'react'
 
 export const getUserDetailsPageContext = cache(async (userId: string) => {
-  const user = await prismaClient.user.findUnique({
-    where: {
-      id: userId,
-    },
-    include: {
-      accounts: true,
-      sessions: true,
-      uploads: true,
-      image: true,
-    },
-  })
-  const bases = await prismaClient.base.findMany({
-    where: {
-      members: {
-        some: {
-          memberId: userId,
+  const [user, bases, resources] = await Promise.all([
+    prismaClient.user.findUnique({
+      where: {
+        id: userId,
+      },
+      include: {
+        accounts: true,
+        sessions: true,
+        uploads: true,
+        image: true,
+      },
+    }),
+    prismaClient.base.findMany({
+      where: {
+        members: {
+          some: {
+            memberId: userId,
+          },
+        },
+        deleted: null,
+      },
+      select: {
+        _count: {
+          select: { members: { where: { member: { deleted: null } } } },
+        },
+        id: true,
+        title: true,
+        slug: true,
+        members: {
+          select: { memberId: true, isAdmin: true },
+          where: { member: { deleted: null } },
         },
       },
-      deleted: null,
-    },
-    select: {
-      _count: {
-        select: { members: { where: { member: { deleted: null } } } },
+    }),
+    prismaClient.resource.findMany({
+      where: {
+        deleted: null,
+        OR: [
+          { createdById: userId },
+          { contributors: { some: { contributorId: userId } } },
+          {
+            base: {
+              deleted: null,
+              members: {
+                some: {
+                  memberId: userId,
+                  accepted: { not: null },
+                },
+              },
+            },
+          },
+        ],
       },
-      id: true,
-      title: true,
-      slug: true,
-      members: {
-        select: { memberId: true, isAdmin: true },
-        where: { member: { deleted: null } },
+      select: {
+        id: true,
+        title: true,
+        slug: true,
+        created: true,
+        lastPublished: true,
+        createdById: true,
+        contributors: {
+          where: { contributorId: userId },
+          select: { contributorId: true },
+        },
+        base: {
+          select: {
+            id: true,
+            title: true,
+            slug: true,
+            members: {
+              where: {
+                memberId: userId,
+                accepted: { not: null },
+              },
+              select: { memberId: true },
+            },
+          },
+        },
       },
-    },
-  })
+      orderBy: [{ lastPublished: 'desc' }, { created: 'desc' }],
+    }),
+  ])
 
-  return { user, bases }
+  return { user, bases, resources }
 })
 
 export type UserDetailsPageContext = Awaited<
